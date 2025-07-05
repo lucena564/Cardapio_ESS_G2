38c176d09a6c50c5f0b5b7c62966fdfb0824789a
AndreVVoigt <andrevoigt98@gmail.com>
2025-07-05 10:29:44 -0300

    feat(backend): Implementa API de gerenciamento de itens

--- backend/src/api/router.py
+++ backend/src/api/router.py
@@ -1,5 +1,17 @@
 from fastapi import APIRouter
-from src.api.items import router as items_router
+from src.api import pedidos, cache, items
+from src.api.adm_client import router as adm_client_router
+
+# Esse import era o exemplo do banco que veio junto à configuração do projeto.
+# from src.api import items
 
 api_router = APIRouter()
-api_router.include_router(items_router, prefix="/items", tags=["items"])
+
+# Esse é o router exemplo do banco que veio junto à configuração do projeto.
+# api_router.include_router(items.router, prefix="/items", tags=["items"])
+
+api_router.include_router(pedidos.router, prefix="/pedidos", tags=["pedidos"])
+
+api_router.include_router(cache.router, prefix="/clear", tags=["pedidos"])
+
+api_router.include_router(adm_client_router, prefix="/admin", tags=["admin"])
--- backend/src/service/meta/item_service_meta.py
+++ backend/src/service/meta/item_service_meta.py
@@ -1,11 +1,11 @@
 
 from abc import ABC, abstractmethod
 
-from src.schemas.item import ItemGet
+from src.schemas.item import Produto
 
 class ItemServiceMeta(ABC):
 
     @abstractmethod
-    def get_item(self, item_id: str) -> ItemGet:
+    def get_item(self, item_id: str) -> Produto:
         """Get item by id method definition"""
         pass
