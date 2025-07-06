96dd5f0f57c6d6ce9325f3ad95395806c8dbbe50
jvictornobre27 <jvictornobre27@gmail.com>
2025-07-05 22:26:59 -0300

    Adicionando ENDPOINTS para categorias mo router.py

--- backend/src/api/router.py
+++ backend/src/api/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from src.api import pedidos, cache
+from src.api import pedidos, cache, categorias
 # Esse import era o exemplo do banco que veio junto à configuração do projeto.
 # from src.api import items
 
@@ -11,3 +11,6 @@ api_router = APIRouter()
 api_router.include_router(pedidos.router, prefix="/pedidos", tags=["pedidos"])
 
 api_router.include_router(cache.router, prefix="/clear", tags=["pedidos"])
+
+#ENDPOINTS destinados a categorias
+api_router.include_router(categorias.router, prefix="/categorias", tags=["categorias"])
